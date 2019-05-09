5c363468f61325482ee941397527810c7858f35e
steadfasterX <steadfasterX@gmail.com>
2019-05-09 12:51:32 +0200

    g4-common: prop: fix graphic glitches
    on skiagl in some rare situations graphic bugs occur
    so we enforce opengl for now
    
     * adb shell setprop debug.hwui.renderer skiagl
     * adb shell setprop debug.hwui.renderer skiavk
       (buggy here as FCs on some apps)
     * adb shell setprop debug.hwui.renderer opengl
       (can be named anything as if the both other doesnt match if fallback to opengl)
    
    Change-Id: I73edc7e380e869e0241d976873322786bac5b98c
    source: frameworks/base/libs/hwui/Properties.cpp
    (cherry picked from commit 4340be9dd0bcd9eaa3807609fdfa0e94c9fbf3e5)

--- g4.mk
+++ g4.mk
@@ -45,5 +45,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=3m \
     dalvik.vm.heapmaxfree=12m
 
+# Rendering: Fix graphical glitches on skiagl
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.renderer=opengl
+
 # Inherits fdroid
 $(call inherit-product-if-exists, vendor/fdroid/fdroid-vendor.mk)
